5a60bdb4db6da5eadc9186c7fc45857fb3eca182
Nathan Clack <clackn@janelia.hhmi.org>
2013-03-15 18:00:48 -0400

    ndio plugins as nd package components

--- app/export/cmake/boost-config.cmake
+++ app/export/cmake/boost-config.cmake
@@ -57,11 +57,12 @@ endif()
 get_target_property(BOOST_ROOT_DIR boost _EP_INSTALL_DIR)
 #show(BOOST_ROOT_DIR)
 #set(BOOST_LIBRARIES PARENT_SCOPE)
-foreach(_lib chrono context date_time exception filesystem graph iostreams locale
-             math_c99 math_c99f math_c99l math_tr1 math_tr1f math_tr1l
-             prg_exec_monitor program_options python random regex serialization
-             signals system test_exec_monitor thread timer unit_test_framework
-             wave wserialization
+foreach(_lib program_options
+  # chrono context date_time exception filesystem graph iostreams locale
+  # math_c99 math_c99f math_c99l math_tr1 math_tr1f math_tr1l
+  # prg_exec_monitor program_options python random regex serialization
+  # signals system test_exec_monitor thread timer unit_test_framework
+  # wave wserialization
            )
   if(NOT TARGET libboost_${_lib})
     add_library(libboost_${_lib} STATIC IMPORTED)
--- cmake/gtest-config.cmake
+++ cmake/gtest-config.cmake
@@ -1,53 +1,54 @@
 include(ExternalProject)
 include(FindPackageHandleStandardArgs)
 
-macro(gtest_copy_shared_libraries _target)  
-  foreach(_lib libgtest libgtest-main)
-    get_target_property(_name ${_lib}    IMPORTED_LOCATION)
-    add_custom_command(TARGET ${_target} POST_BUILD
-      COMMAND ${CMAKE_COMMAND};-E;copy;${_name};$<TARGET_FILE_DIR:${_target}>)  
-  endforeach()
-endmacro()
+if(NOT GTEST_INCLUDE_DIR) #if this is set, assume gtest location has been overridden in parent
+  if(NOT TARGET gtest)
+    # DOWNLOAD AND BUILD
+    ExternalProject_Add(gtest
+      SVN_REPOSITORY http://googletest.googlecode.com/svn/trunk/
+      INSTALL_COMMAND "" #The gtest project  doesn't support install
+      CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+                 -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+                 -DBUILD_SHARED_LIBS:BOOL=TRUE
+      )
+  endif()
+  ExternalProject_Get_Property(gtest INSTALL_DIR)
+  ExternalProject_Get_Property(gtest SOURCE_DIR)
+  get_target_property(SOURCE_DIR  gtest _EP_SOURCE_DIR)
+  get_target_property(INSTALL_DIR gtest _EP_BINARY_DIR)
 
-if(NOT TARGET gtest)
-  # DOWNLOAD AND BUILD
-  ExternalProject_Add(gtest
-    SVN_REPOSITORY http://googletest.googlecode.com/svn/trunk/
-    INSTALL_COMMAND "" #The gtest project  doesn't support install
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-               -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-               -DBUILD_SHARED_LIBS:BOOL=TRUE
-    )
+  if(NOT TARGET libgtest)
+    add_library(libgtest IMPORTED SHARED)
+    add_library(libgtest-main IMPORTED SHARED)
+  endif()
+
+  set_target_properties(libgtest PROPERTIES  
+    IMPORTED_LINK_INTERFACE_LANGUAGES CXX
+    IMPORTED_IMPLIB   ${INSTALL_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}
+    IMPORTED_LOCATION ${INSTALL_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_SHARED_LIBRARY_PREFIX}gtest${CMAKE_SHARED_LIBRARY_SUFFIX}
+  )
+  set_target_properties(libgtest-main PROPERTIES  
+    IMPORTED_LINK_INTERFACE_LANGUAGES CXX
+    IMPORTED_IMPLIB   ${INSTALL_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtest_main${CMAKE_STATIC_LIBRARY_SUFFIX}
+    IMPORTED_LOCATION ${INSTALL_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_SHARED_LIBRARY_PREFIX}gtest_main${CMAKE_SHARED_LIBRARY_SUFFIX}
+  )
+
+  get_property(GTEST_LIBRARY      TARGET libgtest      PROPERTY LOCATION)
+  get_property(GTEST_MAIN_LIBRARY TARGET libgtest-main PROPERTY LOCATION)
+  set(GTEST_BOTH_LIBRARIES ${GTEST_LIBRARY} ${GTEST_MAIN_LIBRARY})
+  set(GTEST_INCLUDE_DIR ${SOURCE_DIR}/include)
 endif()
-
-get_target_property(GTEST_SRC_DIR gtest _EP_SOURCE_DIR)
-get_target_property(GTEST_ROOT_DIR gtest _EP_BINARY_DIR)
-
-if(NOT TARGET libgtest)
-  add_library(libgtest IMPORTED SHARED)
-  add_library(libgtest-main IMPORTED SHARED)
-endif()
-
-set_target_properties(libgtest PROPERTIES  
-  IMPORTED_LINK_INTERFACE_LANGUAGES CXX
-  IMPORTED_IMPLIB   ${GTEST_ROOT_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}
-  IMPORTED_LOCATION ${GTEST_ROOT_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_SHARED_LIBRARY_PREFIX}gtest${CMAKE_SHARED_LIBRARY_SUFFIX}
-)
-set_target_properties(libgtest-main PROPERTIES  
-  IMPORTED_LINK_INTERFACE_LANGUAGES CXX
-  IMPORTED_IMPLIB   ${GTEST_ROOT_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtest_main${CMAKE_STATIC_LIBRARY_SUFFIX}
-  IMPORTED_LOCATION ${GTEST_ROOT_DIR}/${CMAKE_CFG_INTDIR}/${CMAKE_SHARED_LIBRARY_PREFIX}gtest_main${CMAKE_SHARED_LIBRARY_SUFFIX}
-)
-
-set(GTEST_LIBRARY libgtest)
-set(GTEST_MAIN_LIBRARY libgtest-main)
-set(GTEST_BOTH_LIBRARIES libgtest libgtest-main)
-set(GTEST_INCLUDE_DIR ${GTEST_SRC_DIR}/include)
-
 find_package_handle_standard_args(GTEST DEFAULT_MSG
   GTEST_BOTH_LIBRARIES
   GTEST_INCLUDE_DIR
 )
+
+macro(gtest_copy_shared_libraries _target)  
+  foreach(_lib ${GTEST_BOTH_LIBRARIES})
+    add_custom_command(TARGET ${_target} POST_BUILD
+      COMMAND ${CMAKE_COMMAND};-E;copy;${_lib};$<TARGET_FILE_DIR:${_target}>)  
+  endforeach()
+endmacro()
 
 ### INSTALL
 if(NOT TARGET install-gtest)

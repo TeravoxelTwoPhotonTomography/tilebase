bcbf4aff6fe89aa5a7cceb3918db220d4aaae15a
Nathan Clack <clackn@janelia.hhmi.org>
2014-05-21 10:04:05 -0400

    Dispatch for target-address should work.
      - This required no real change.
    
        worker.js relies on the option parsing behavior of render
        to override the target-address and gpu arguments.
    
        Made a note to that effect in worker.js

--- app/render/util/worker.js
+++ app/render/util/worker.js
@@ -2,15 +2,22 @@
 
 /*
   Usage:
-  
+
     batch.js [addresses] [root command...]
 
-    addresses     A commma seperated list of node addresses in the target octree 
+    addresses     A commma seperated list of node addresses in the target octree
                   Example: 11,12,13,14,15,16,17
 
     root command  The render command that will be used to target each address.
                   Example: ./render in out -x 1 -y 1 -z 2
-*/                
+
+                  Note that the -gpu and --target-address will be overridden
+                  for each gpu specific job.
+
+                  The way arguments are overridden relies on the option parsing
+                  behavior in render; arguments at the end of the command line
+                  override those that come earlier.
+*/
 
 var spawn = require('child_process').spawn,
     map   = require('async').map;
